3206403f8173cf385eba083176e6a99923a8896d
Dylan Waser <waserd@etsu.edu>
2024-01-04 16:47:56 -0800

    edit puppy.go and go get go.mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/waserdyland/puppy
 
 go 1.21.5
+
+require github.com/waserdyland/dog v0.0.0-20240105003623-a7b99a0b11da // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
-func Bark() string{
+import (
+	"github.com/waserdyland/dog"
+)
+
+func Bark() string {
 	return "Woof!"
 }
 
-func Barks() string{
+func Barks() string {
 	return "Woof Woof Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowsUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrowsUp(Barks())
 }
